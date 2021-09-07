1bc37596400f8db14d8e49da526c0feed34bf11b
ulisesten <ulises.mtz.el@gamil.com>
2021-09-06 21:41:55 -0500

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h> //standard input output
+
+/**
+ * Declaración
+ * Definición
+ * LLamada
+*/
+
+/**
+ * Declaración de una función
+ * valor de retorno
+ * nombre
+ * argumentos o parametros
+*/
+
+/**
+ * int = entero
+*/
+
+int suma (int sumando1, int sumando2);
+
+int main() {
+
+    printf("Hola mundo\n");
+    suma(5, 6);
+
+    return 0;
+}
+
+/**
+ * Definición de la función
+ * valor de retorno
+ * nombre
+ * argumentos o parametros
+ * cuerpo
+*/
+
+int suma (int sumando1, int sumando2) {
+    printf("Esta es la funcion suma\n");
+}
+
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int suma (int sumando1, int sumando2);
+
+//int = entero
+//1.8
+
+int main() {
+
+    std::cout << "Hola mundo" << std::endl;
+    suma(6, 8);
+
+    return 0;
+}
+
+int suma (int sumando1, int sumando2) {
+    std::cout << "Hola mundo" << std::endl;
+}
